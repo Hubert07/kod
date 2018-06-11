ae39cd5b2f0912b219b2bd052d67dd0d23d5e4e2
Hubert07 <gajewski.hubert95@gmail.com>
2018-06-11 09:41:00 +0200

    dlotek

--- pyton/dlotek.py
+++ pyton/dlotek.py
@@ -12,11 +12,25 @@ def main(args):
 
 # losowanie
     liczby = []  # lista wylosowanych liczb
-    for i in range(ileliczb):
-        liczba = random.randint(1, maksliczba)
-        if liczby.count(liczba) == 0:
+    i = 0
+# for i in range(ileliczb):
+    while i < ileliczb:
+        liczba = random.randint(1, maksliczba + 1)
+        if liczby.count(liczba) == 0:  # sprawdzenie czy liczba już jest
             liczby.append(liczba)
+            i += 1  # powiększ i o 1
     print(liczby)
+
+    typy = set()  # dekPlarowanie pustego zbioru na typy użytkownika
+    i = 0
+    while i < ileliczb:
+        typ = input("Podaj liczbę {}:".format(i + 1))
+        if typ not in typy:  # jeżeli podanego typu nei ma w zbiorze
+            typy.add(typ)  # dodaj typ uzytkownika
+            i += 1  # dodaj do zbioru
+    print(typy)
+
+    if typy ==
 
     # if liczba == int(odp):
     #     print('Umiesz w liczby')
