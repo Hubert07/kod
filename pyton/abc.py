722318cd37813ef8cf98e7564af643b7d9c1961b
Hubert07 <gajewski.hubert95@gmail.com>
2018-09-19 07:54:48 +0200

    algorytm sprawdzający boki trójkąta i wybierający największą liczbę

--- pyton/abc.py
+++ pyton/abc.py
@@ -8,18 +8,53 @@
 # a następnie wyświetla liczbę największą można używać and/or
 
 
-def max(a, b, c)
+def max(a, b, c):
+    """
+    Funkcja wyszukuje i zwraca największą z 3 liczb
+    """
     m = None
     if a > b:
         if a > c:
             m = a
+
+    elif b > a and b > c:
+            m = b
+
+    else:
+        m = c
     print("Największa: ", m)
+
     return m
+
+
+def max2(a, b, c):
+    m = a  # funkcja zakłada, żse największą liczbą jest a (potem to się zmieni)
+    if b > m:
+        m = b
+    if c > m:
+        m = c
+    return m
+
+
+# def max3(a, b, c):
+#     m = None
+#     if a / 2 + b / 2 < c
+#     print("Największa: ", c)
 
 
 def main(args):
 
-    assert(maks(1, 2, 3) == 3)
+    assert(max2(1, 2, 3) == 3)
+    assert(max2(1, 3, 2) == 3)
+    assert(max2(1, 1, 3) == 3)
+    assert(max2(2, 1, 3) == 3)
+    assert(max2(2, 3, 1) == 3)
+    assert(max2(2, 2, 3) == 3)
+    assert(max2(3, 2, 1) == 3)
+    assert(max2(3, 1, 2) == 3)
+    assert(max2(3, 3, 2) == 3)
+    assert(max2(3, 3, 1) == 3)
+    assert(max2(3, 3, 3) == 3)
     # a = int(input("Podaj pierwszą liczbę: "))
     # b = int(input("Podaj drugą liczbę: "))
     # c = int(input("Podaj trzecią liczbę: "))
