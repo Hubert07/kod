57651108c4904b163e89d9bc9687309994d33bd0
Hubert07 <gajewski.hubert95@gmail.com>
2018-10-01 20:39:10 +0200

    praca domowa cz1

---
+++ pyton/cwc_petle3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  cwc_petle3.py
+#
+
+
+def main(args):
+	start = int(input(0))
+	stop = int(input("Druga liczba: "))
+
+	while a > 0
+	if start < stop:
+		for liczba in range(start, (stop + 1) * (stop + 1):
+			print(liczba, " ", end="")
+	return 0
+
+if __name__ == '__main__':
+	import sys
+	sys.exit(main(sys.argv))
