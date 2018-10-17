f4a01ef1dd5dd71725df41ed2c2039fb0fdf346a
Hubert07 <gajewski.hubert95@gmail.com>
2018-10-17 16:22:52 +0200

    bo mi się repo nie chce zakrualizowac

--- cpp/tabliczka_mnozenia.cpp
+++ cpp/tabliczka_mnozenia.cpp
@@ -7,11 +7,11 @@ void tabliczka(int a, int b) {
  // void = funkcja nie zwraca wartości
     for (int i = 1; i <= a; i++) {
         for (int j = 1; j <= b; j++) {
-            cout << setw(4) << i * j;
+             cout << setw(4) << i * j;
             }
+         cout << endl;
         }
 // for (start; warunek kończący; co się ma stac jak pętla zrobi 'obrót')
-    ;
     }
 
 int main(int argc, char **argv) {
