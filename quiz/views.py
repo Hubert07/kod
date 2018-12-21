390d27a446cda392b3910224f2d09f1293bc7614
Hubert07 <gajewski.hubert95@gmail.com>
2018-12-21 08:45:06 +0100

    quiz rozbudowa

--- quiz/views.py
+++ quiz/views.py
@@ -6,6 +6,7 @@
 
 from flask import Flask
 from flask import render_template, request
+from flask import redirect, url_for
 from modele import *
 
 app = Flask(__name__)
@@ -23,7 +24,19 @@ def lista():
     return render_template('lista.html', query=pytania)
 
 
-@app.route("/quiz")
+@app.route("/quiz", methods=['GET', 'POST'])
 def quiz():
-    pytania = Pytanie.select().join(Odpowiedz).distinct()
+    if request.method == 'POST':
+        print(request.form)
+        wynik = 0
+        for pid, oid in request.form.items():
+            odp = Odpowiedz().get(Odpowiedz.id == int(oid)).odpok
+            if odp:
+                wynik += 1
+        print("Poprawne", wynik)
+        flash("Wynik: {}".format(wynik), 'info')
+        return redirect(url_for('index'))
+
+
+    pytania = Pytanie.select().join(Odpowiedz).distinct().order_by(Pytanie.id)
     return render_template('quiz.html', query=pytania)
