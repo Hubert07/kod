2527ecd12c5a7571395249d766b5b9618f9b3f25
Hubert07 <gajewski.hubert95@gmail.com>
2019-01-04 08:34:51 +0100

    quiz aplikacja itd

--- quiz/views.py
+++ quiz/views.py
@@ -6,7 +6,7 @@
 
 from flask import Flask
 from flask import render_template, request
-from flask import redirect, url_for
+from flask import redirect, url_for, flash
 from modele import *
 
 app = Flask(__name__)
@@ -30,13 +30,11 @@ def quiz():
         print(request.form)
         wynik = 0
         for pid, oid in request.form.items():
-            odp = Odpowiedz().get(Odpowiedz.id == int(oid)).odpok
-            if odp:
+            if Odpowiedz().get(Odpowiedz.id == int(oid)).odpok:
                 wynik += 1
         print("Poprawne", wynik)
         flash("Wynik: {}".format(wynik), 'info')
         return redirect(url_for('index'))
-
 
     pytania = Pytanie.select().join(Odpowiedz).distinct().order_by(Pytanie.id)
     return render_template('quiz.html', query=pytania)
