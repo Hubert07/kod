9af5540aa58614baeee2595b61789de2234864d2
Hubert07 <gajewski.hubert95@gmail.com>
2019-01-11 08:45:30 +0100

    dodawanie pytania

--- quiz/views.py
+++ quiz/views.py
@@ -8,6 +8,7 @@ from flask import Flask
 from flask import render_template, request
 from flask import redirect, url_for, flash
 from modele import *
+from forms import *
 
 app = Flask(__name__)
 print(__name__)
@@ -38,3 +39,9 @@ def quiz():
 
     pytania = Pytanie.select().join(Odpowiedz).distinct().order_by(Pytanie.id)
     return render_template('quiz.html', query=pytania)
+
+
+@app.route("/dodaj", methods=['GET', 'POST'])
+def dodaj():
+    form = PytanieForm()
+    return render_template('dodaj.html', form=form)
