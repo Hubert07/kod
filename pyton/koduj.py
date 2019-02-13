9b1a93c10b916973089dbfd944783a6f1b318b6b
Hubert07 <gajewski.hubert95@gmail.com>
2019-02-13 11:41:58 +0100

    python mors

---
+++ pyton/koduj.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  kompresja.py
+#
+
+kod = {
+    'a': '+-',
+    'b': '-+++'
+}
+
+
+def koduj():
+
+
+def main(args):
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
